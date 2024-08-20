f526cc21672e12fef3324a0e55a053a85e5f0607
Whit Allee <whitallee@gmail.com>
2024-08-19 20:50:16 -0500

    WIP flashcard-set

--- app/flashcard-set/page.js
+++ app/flashcard-set/page.js
@@ -4,33 +4,36 @@ import { useUser } from "@clerk/nextjs";
 import { database } from "../../firebase";
 import { useState, useEffect } from "react";
 import { collection, doc, getDoc, setDoc } from "firebase/firestore";
-import { redirect } from "next/navigation";
+import { redirect, useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { Card, CardActionArea, CardContent, Container, Grid, Typography } from "@mui/material";
 
-export default function FlashcardSets() {
+export default function FlashcardSet() {
     const { isLoaded, isSignedIn, user } = useUser()
+
+    const searchParams = useSearchParams()
+    const fcSetName = searchParams.get('name')
 
     const router = useRouter()
 
-    const [flashcardSets, setFlashcardSets] = useState([])
+    const [flashcards, setFlashcards] = useState([])
 
     useEffect(() => {
-        async function getFlashcardSets(){
-            if(!user) return
+        async function getFlashcards(){
+            if(!fcSetName || !user) return
             const docRef = doc(collection(database, 'users'), user.id)
             const docSnap = await getDoc(docRef)
 
             if(docSnap.exists()){
-                const collections = docSnap.data().flashcardSets || []
+                const collections = docSnap.data().flashcardSets[fcSetName] || []
                 // console.log(collections)
-                setFlashcardSets(collections)
+                setFlashcards(collections)
             } else {
-                await setDoc(docRef, {flashcardSets: []})
+                // await setDoc(docRef, {flashcards: []})
             }
         }
 
-        getFlashcardSets()
+        getFlashcards()
     }, [user])
 
     // if (!isLoaded || !isSignedIn) {
@@ -51,13 +54,13 @@ export default function FlashcardSets() {
     return (
         <Container maxWidth="100vw">
             <Grid container spacing={3} sx={{mt: 4}}>
-                {flashcardSets.map((flashcardSet, index) => {
+                {flashcards.map((flashcard, index) => {
                     return (
                     <Grid item xs={12} sm={6} md={4} key={index}>
                         <Card>
-                            <CardActionArea onClick={()=>handleCardClick(flashcardSet.name)}>
+                            <CardActionArea onClick={()=>handleCardClick(flashcard.name)}>
                             <CardContent>
-                                <Typography variant="h6">{flashcardSet.name}</Typography>
+                                <Typography variant="h6">{flashcard.name}</Typography>
                             </CardContent>
                             </CardActionArea>
                         </Card>
